90f8f5df4b40685585154483431e913e63ed2fac
George Jahad <github@blackbirdsystems.net>
2009-11-22 13:28:40 -0800

    eval-with-context-fn sort of works

--- src/clj/clojure/debug.clj
+++ src/clj/clojure/debug.clj
@@ -1,16 +1,14 @@
-(ns clojure.debug)
+(ns clojure.debug
+  (:require clojure.main))
 
 (defmacro defn-debug
-  "Just like defn, but turns on lexical frame creation"
+  "Like defn, but turns on lexical frame creation"
   [fn-name & defn-stuff]
   `(do
-     ;;?? alter?
-     (alter-var-root #'clojure.core/*create-lexical-frames*
-                     (fn [~'c ~'d] ~'d)  true)
-     (defn ~fn-name ~@defn-stuff)
-     (alter-var-root #'clojure.core/*create-lexical-frames*
-                     (fn [~'c ~'d] ~'d) false)
-     nil))
+     (push-thread-bindings {#'clojure.core/*create-lexical-frames* true})
+     (try
+      (defn ~fn-name ~@defn-stuff)
+      (finally (pop-thread-bindings)))))
 
 (defmacro get-context [context]
   `(def ~context (get-thread-bindings)))
@@ -33,3 +31,38 @@
             ~form))
          (finally (pop-thread-bindings))))
      (finally (pop-thread-bindings)))))
+
+(defmacro gen-eval-with-context [context]
+  (do
+    ;;eval??
+    (push-thread-bindings (eval context))
+    (try
+     `(do
+        (push-thread-bindings ~context)
+        (try
+         (fn [form#]
+           (eval
+            (let [~@(make-let-bindings)]
+              form#)))
+         (finally (pop-thread-bindings))))
+     (finally (pop-thread-bindings)))))
+
+(def context-var nil)
+(def form-var nil)
+
+(defn eval-with-context-fn [context form]
+  (do
+    (alter-var-root #'clojure.debug/context-var
+                    (fn [c d] d)  context)
+    (alter-var-root #'clojure.debug/form-var
+                    (fn [c d] d)  form)
+    (println "c f" context-var form-var)
+    (println (user/macroexpand-r `(eval-with-context context-var ~form-var)))
+    (eval (eval `(eval-with-context context-var ~form-var)))
+    (alter-var-root #'clojure.debug/form-var
+                    (fn [c d] d)  nil)
+    (alter-var-root #'clojure.debug/context-var
+                    (fn [c d] d)  nil)))
+
+(defmacro debug-repl [context form]
+  (clojure.main/repl :eval  ))
