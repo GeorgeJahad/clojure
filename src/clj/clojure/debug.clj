7162e3cea62a72b3306af40c98473f2c0747a373
George Jahad <github@blackbirdsystems.net>
2009-12-06 10:43:25 -0800

    code rearrangement

--- src/clj/clojure/debug.clj
+++ src/clj/clojure/debug.clj
@@ -7,37 +7,12 @@
 ;; software.
 
 ;; Contributed by George Jahad
+;; This code properly belongs in clojure.contrib, but I since I had to
+;; hack the compiler anyway I decided to put it here for now, just to
+;; minimize the number of patched repos users need.
 
 (ns clojure.debug
   (:require clojure.main))
-
-;; dynamic bindings don't seem to work at compile time so I had to use
-;; alter-var-root.  with-alter-var-root doesn't work because it is
-;; nested in one big let block and compiler granularity requires
-;; it to be in a "do" block
-
-(defn alter-helper [a b] b)
-
-(defmacro with-lexical-frames
-  "Turns on lexical frame creation"
-  [& body]
-  `(do
-     (alter-var-root #'clojure.core/*create-lexical-frames* alter-helper true)
-     (try
-      ~@body
-      (finally
-       (alter-var-root #'clojure.core/*create-lexical-frames*
-                       alter-helper false)))))
-
-(defmacro defn-debug
-  [fn-name & defn-stuff]
-  `(with-lexical-frames
-     (defn ~fn-name ~@defn-stuff)))
-
-(defmacro deftest-debug
-  [test-name & deftest-stuff]
-  `(with-lexical-frames
-     (deftest ~test-name ~@deftest-stuff)))
 
 (defstruct context-struct :bindings :lexical-frames)
 
@@ -83,3 +58,31 @@
      `(clojure.main/repl :prompt #(print "dr => ")
                          :eval (partial eval-with-context-fn '~context))))
 
+
+;; dynamic bindings don't seem to work at compile time so I had to use
+;; alter-var-root.  with-alter-var-root doesn't work because it is
+;; nested in one big let block and compiler granularity requires
+;; it to be in a "do" block
+
+(defn alter-helper [a b] b)
+
+(defmacro with-lexical-frames
+  "Turns on lexical frame creation"
+  [& body]
+  `(do
+     (alter-var-root #'clojure.core/*create-lexical-frames* alter-helper true)
+     (try
+      ~@body
+      (finally
+       (alter-var-root #'clojure.core/*create-lexical-frames*
+                       alter-helper false)))))
+
+(defmacro defn-debug
+  [fn-name & defn-stuff]
+  `(with-lexical-frames
+     (defn ~fn-name ~@defn-stuff)))
+
+(defmacro deftest-debug
+  [test-name & deftest-stuff]
+  `(with-lexical-frames
+     (deftest ~test-name ~@deftest-stuff)))
